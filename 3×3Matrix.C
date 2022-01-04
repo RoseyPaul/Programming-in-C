ef749f0220552bda3eb83edf3ee95f4b16cc11ca
RosyPaul <96780614+RoseyPaul@users.noreply.github.com>
2022-01-04 21:01:57 +0530

    Update 3×3Matrix.C

--- 3×3Matrix.C
+++ 3×3Matrix.C
@@ -13,7 +13,7 @@ void main()
               }
         }
             printf("The Matrix:\n");
-            for(i=0;i<2;i++)
+            for(i=0;i<3;i++)
               {
                 for(j=0;j<3;j++)
                  {
